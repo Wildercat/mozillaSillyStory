4b8c0d9e5be8f92d85d64a825adf1556d53f6703
Glitch (wildercat-mozillasillystory) <none>
2019-09-06 18:16:02 +0000

    add api call for randon name

--- main.js
+++ main.js
@@ -5,15 +5,7 @@ var story = document.querySelector('.story');
 function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
-/*
-async function fetchUsr() {
-  let usrJson = await fetch('https://randomuser.me/api/')
-    .then(response => response.json());
-  return usrJson;
-}
-*/
 
-async function main() {
 
 var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 var insertX = [
@@ -34,7 +26,7 @@ var insertZ = [
 
 randomize.addEventListener('click', result);
 
-function result() {
+async function result() {
     var newStory = storyText;
     var xItem = randomValueFromArray(insertX);
     var yItem = randomValueFromArray(insertY);
@@ -48,10 +40,11 @@ function result() {
   if(customName.value !== '') {
     var name = customName.value;
     newStory = newStory.replace('Bob', name);
-  } else {
+  } else { // get first name from randomuser database if no name is specified
     let usrJson = await fetch('https://randomuser.me/api/')
     .then(response => response.json());
-    console.log()
+    var name = usrJson.results[0].name.first;
+    newStory = newStory.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
@@ -65,6 +58,3 @@ function result() {
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
-}
-
-main();
