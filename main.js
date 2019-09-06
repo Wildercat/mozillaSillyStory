ef028593668720df9d46a2ffe84a4baf827feb5b
Glitch (wildercat-mozillasillystory) <none>
2019-09-06 17:08:55 +0000

    🐩👮 Checkpoint ./main.js:5242288/707

--- main.js
+++ main.js
@@ -6,16 +6,12 @@ function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
 
-fetch('https://randomuser.me/api/')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    var randUser = myJson;
-  })
-
-console.log(randUser);
-
+async function fetchUsr() {
+  let usrJson = await fetch('https://randomuser.me/api/')
+    .then(response => response.json());
+  return usrJson;
+}
+console.log(fetchUsr());
 var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 var insertX = [
     'Willy the Goblin',
